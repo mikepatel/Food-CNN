edb63ce08d4cd917b70a692c0fe6a334c13edea1
Michael <michael.patel94@gmail.com>
2021-04-02 22:08:10 -0400

    Mobilenet S

--- train2.py
+++ train2.py
@@ -38,7 +38,9 @@ if __name__ == "__main__":
 
     # image generators
     datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-        #rescale=1./255,
+        rescale=1./255,
+        horizontal_flip=True,
+        vertical_flip=True,
         validation_split=VALIDATION_SPLIT
     )
 
@@ -75,10 +77,9 @@ if __name__ == "__main__":
     """
     inputs = tf.keras.Input(shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS))
     x = inputs
-    x = tf.keras.applications.mobilenet_v2.preprocess_input(x)
     x = mobilenet(x)
     x = tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
-    x = tf.keras.layers.Dropout(rate=0.2)(x)
+    x = tf.keras.layers.Dropout(rate=0.5)(x)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dense(units=num_classes, activation="softmax")(x)
     outputs = x
@@ -131,6 +132,61 @@ if __name__ == "__main__":
     plt.xlabel('epoch')
 
     plt.savefig(os.path.join(os.getcwd(), "plots"))
+
+    # ----- FINE TUNE ----- #
+    print(f'\n\nFINE TUNE\n\n')
+
+    # fine-tune model
+    # unfreeze base
+    mobilenet.trainable = True
+    fine_tune_at = 100
+    for layer in mobilenet.layers[:fine_tune_at]:
+        layer.trainable = False  # freeze early layers
+
+    # re-compile model
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer=tf.keras.optimizers.SGD(learning_rate=LEARNING_RATE_FINE_TUNING),
+        metrics=["accuracy"]
+    )
+
+    model.summary()
+
+    # continue training
+    history = model.fit(
+        x=train_generator,
+        steps_per_epoch=len(train_generator),
+        epochs=NUM_EPOCHS_FINE_TUNING,
+        validation_data=validation_generator,
+        validation_steps=len(validation_generator)
+    )
+    
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    plt.figure(figsize=(8, 8))
+    plt.grid()
+    plt.subplot(2, 1, 1)
+    plt.plot(acc, label='Training Accuracy')
+    plt.plot(val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.ylabel('Accuracy')
+    plt.ylim([min(plt.ylim()), 1])
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(loss, label='Training Loss')
+    plt.plot(val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.ylabel('Cross Entropy')
+    plt.ylim([0, 3.0])
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+
+    plt.savefig(os.path.join(os.getcwd(), "plots_finetune"))
 
     # save model
     model.save(SAVE_DIR)
